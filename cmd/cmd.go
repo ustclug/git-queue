bd024fd02b2303dc59b8c958d2a8d9f2017ab66d
iBug <git@ibugone.com>
2026-04-30 00:59:57 +0800

    Implement "server" subcommand

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/ustclug/git-queue/pkg/server"
 )
 
 func ServerCmd() *cobra.Command {
@@ -9,9 +10,16 @@ func ServerCmd() *cobra.Command {
 		Use:   "server",
 		Short: "Run as server",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, _ []string) error {
-			return nil
-		},
+	}
+	config := server.DefaultConfig()
+	config.InstallFlags(c.Flags())
+	c.RunE = func(cmd *cobra.Command, _ []string) error {
+		s := server.NewServer(config)
+		if err := s.Start(); err != nil {
+			return err
+		}
+		<-make(chan struct{})
+		return nil
 	}
 	return c
 }
